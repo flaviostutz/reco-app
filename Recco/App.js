2a5a2e7705e87f3fa744a45f40d97190dfe02d78
flaviostutz <flaviostutz@gmail.com>
2020-04-28 21:24:34 -0300

    cleanup. found timing sync issues when changing component sizes

--- Recco/App.js
+++ Recco/App.js
@@ -24,6 +24,7 @@ import {
 } from 'react-native';
 
 import CameraRoll from "@react-native-community/cameraroll";
+import CameraRollPicker from 'react-native-camera-roll-picker';
 
 import {
   Header,
@@ -46,7 +47,7 @@ class App extends Component {
 
   state = {
     paused: true,
-    recording: false,
+    state: 'idle',
 
     flash: RNCamera.Constants.FlashMode.off,
     type: RNCamera.Constants.Type.front,
@@ -62,16 +63,14 @@ class App extends Component {
     },
 
     lastRecording: null,
+    lastRecordingStartTime: null,
 
-    lastRecordingTime: null,
-    lastRecordingActualTime: null,
-
-    // recordingTriggerTime: 800,
+    videoPicker: false,
+    playbackVideo: require('./res/test-30fps-360p.mp4'),
+    saveToCameraRoll: true,
 
     lagCounter: 0,
     lagTotal: 0,
-
-    isRecording: false,
   };  
 
 
@@ -81,75 +80,76 @@ class App extends Component {
     return (
       <>
         <StatusBar barStyle="dark-content" />
+        {this.state.videoPicker &&
+        <CameraRollPicker callback={this.onVideoSelected} 
+                          assetType="Videos"
+                          selectSingleItem={true} />
+        }
+        {!this.state.videoPicker &&
         <SafeAreaView>
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
             style={styles.scrollView}>
-            {/* <Header /> */}
-            {/* {global.HermesInternal == null ? null : (
-              <View style={styles.engine}>
-                <Text style={styles.footer}>Engine: Hermes</Text>
-              </View>
-            )} */}
             <View style={styles.body}>
               <View style={styles.sectionContainer}>
-                <Text style={styles.sectionTitle}>Click on video to start</Text>
-                <Text style={styles.sectionDescription}>
+                <Text style={styles.sectionTitle}>Recco</Text>
+                {/* <Text style={styles.sectionDescription}>
                   As soon as you click on a video it will start playing and a new recording will
                   take place
-                </Text>
+                </Text> */}
               </View>
               <View style={styles.sectionContainer}>
-                <RNCamera
-                  ref={ref => {
-                    this.camera = ref;
-                  }}
-                  style={styles.video1}
-
-                  //camera recording config
-                  type={this.state.type}
-                  ratio={this.state.ratio}
-                  flashMode={this.state.flash}
-
-                  //events
-                  onAudioInterrupted={this.onAudioInterrupted}
-                  onAudioConnected={this.onAudioConnected}
-                  onRecordingStart={this.onRecordingStart}
-                  onRecordingEnd={this.onRecordingEnd}
-                  onCameraReady={this.onCameraReady}
-                  onMountError={this.onCameraMountError}
-
-                  //camera authorization matters
-                  onStatusChange={this.onCameraStatusChange}
-                  androidCameraPermissionOptions={{
-                    title: 'Permission to record camera video',
-                    message: 'We need access to your camera in order to record your track',
-                    buttonPositive: 'Ok',
-                    buttonNegative: 'Cancel',
-                  }}
-                  androidRecordAudioPermissionOptions={{
-                    title: 'Permission to record audio',
-                    message: 'We need access to your microphone in order to record your track',
-                    buttonPositive: 'Ok',
-                    buttonNegative: 'Cancel',
-                  }}
-                  pendingAuthorizationView={
-                    <SafeAreaView style={styles.cameraLoading}>
-                      <Spinner color={Colors.gray}/>
-                    </SafeAreaView>
-                  }
-                  notAuthorizedView={
-                    <View>
-                      Recco was not authorized to access media
-                    </View>
-                  }
-                >
-                </RNCamera>
-              </View>
-              <View style={styles.sectionContainer}>
-                {/* <TouchableOpacity onPress={() => {this.setState({paused: !this.state.paused})}}> */}
                 <TouchableOpacity onPress={() => {this.toggleRecording()}}>
-                  <Video source={require('./res/test-30fps-360p.mp4')}   // Can be a URL or a local file.
+                  <RNCamera
+                    ref={ref => {
+                      this.camera = ref;
+                    }}
+                    style={styles.camera}
+
+                    //camera recording config
+                    type={this.state.type}
+                    ratio={this.state.ratio}
+                    flashMode={this.state.flash}
+
+                    //events
+                    onAudioInterrupted={this.onAudioInterrupted}
+                    onAudioConnected={this.onAudioConnected}
+                    onRecordingStart={this.onRecordingStart}
+                    onRecordingEnd={this.onRecordingEnd}
+                    onCameraReady={this.onCameraReady}
+                    onMountError={this.onCameraMountError}
+
+                    //camera authorization matters
+                    onStatusChange={this.onCameraStatusChange}
+                    androidCameraPermissionOptions={{
+                      title: 'Permission to record camera video',
+                      message: 'We need access to your camera in order to record your track',
+                      buttonPositive: 'Ok',
+                      buttonNegative: 'Cancel',
+                    }}
+                    androidRecordAudioPermissionOptions={{
+                      title: 'Permission to record audio',
+                      message: 'We need access to your microphone in order to record your track',
+                      buttonPositive: 'Ok',
+                      buttonNegative: 'Cancel',
+                    }}
+                    pendingAuthorizationView={
+                      <SafeAreaView style={styles.cameraLoading}>
+                        <Spinner color={Colors.gray}/>
+                      </SafeAreaView>
+                    }
+                    notAuthorizedView={
+                      <View>
+                        Recco was not authorized to access media
+                      </View>
+                    }
+                  >
+                  </RNCamera>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.sectionContainer}>
+                <TouchableOpacity onPress={() => {this.selectVideo()}}>
+                  <Video source={this.state.playbackVideo}   // Can be a URL or a local file.
                       ref={(ref) => {
                         this.player1 = ref
                       }}                                      // Store reference
@@ -164,6 +164,7 @@ class App extends Component {
                       style={styles.video1}
                       playWhenInactive={true}
                       playInBackground={true}
+                      pictureInPicture={true}
                       volume={1.0} />
                 </TouchableOpacity>
               </View>
@@ -172,24 +173,38 @@ class App extends Component {
                 <View style={styles.sectionContainer}>
                   <Video source={{uri: this.state.lastRecording.uri}}   // Can be a URL or a local file.
                         ref={(ref) => {
-                          this.player3 = ref
+                          this.player2 = ref
                         }}                                      // Store reference
                         paused={this.state.paused}
                         onBuffer={this.onBuffer}                // Callback when remote video is buffering
                         onError={this.videoError}               // Callback when video cannot be loaded
-                        style={styles.video3} />
+                        style={styles.video2} />
                 </View>
               </TouchableOpacity>
               }
             </View>
           </ScrollView>
         </SafeAreaView>
+        }
       </>
     );
   }
 
+  onVideoSelected = async (images, current) => {
+    console.log("VIDEO URI: " + current.uri)
+    const ext='mp4'
+    const appleId = current.uri.substring(5, 41);
+    const uri = `assets-library://asset/asset.${ext}?id=${appleId}&ext=${ext}`;
+    this.setState({videoPicker: false, playbackVideo: {uri: uri}})
+  }
+
+  selectVideo = async () => {
+
+    this.setState({videoPicker: true})
+  }
+
   toggleRecording = async () => {
-    if (this.state.recording) {
+    if (this.state.state != 'idle') {
       this.stopRecording()
     } else {
       this.startRecording()
@@ -197,66 +212,42 @@ class App extends Component {
   }
   
   startRecording = async () => {
-    if (this.camera && !this.state.recording) {
-      this.setState({lastRecording: null})
-      console.log("START RECORDING!")
+    if (this.camera && this.state.state=='idle') {
+      this.setState({state:'preparing', lastRecording: null}, async () => {
+        console.log("START RECORDING")
 
-      // need to do this in order to avoid race conditions
-      this.state.recording = true;
+        const options = {
+          quality: '480p',
+          maxDuration: 300,
+          maxFileSize: 200 * 1024 * 1024
+        };
 
-      const options = {
-        quality: '480p',
-        maxDuration: 5,
-        maxFileSize: 100 * 1024 * 1024
-       };
-
-
-      this.setState({recording: true, elapsed: -1}, async () => {
-
-        let result = null;
-        console.log("BEGIN RECORD NOW " + new Date().getTime())
-        // setTimeout(()=>{
-        //   this.startVideo()
-        // }, 5);
-        this.state.lastRecordingTime = new Date()
-
-        // var startVideoDelay = this.state.recordingTriggerTime - 200
         var startVideoDelay = 500
         if(this.state.lagCounter>0) {
           startVideoDelay = (this.state.lagTotal / this.state.lagCounter) + 500
         }
-        console.log("USING DELAY " + startVideoDelay + "ms")
 
-        console.log(new Date().getTime() + ' launch camera recording')
+        console.log(new Date().getTime() + ' starting recording')
         this.camera.recordAsync(options).then((result) => {
-          //camera.stopRecording
-          console.log("FINISHED RECORD NOW " + new Date().getTime())
+          console.log(new Date().getTime() + " finished recording")
+          this.stopVideo()
 
-          if(result) {
-            Alert.alert("Video recorded!", JSON.stringify(result));
-            this.stopVideo()
-            this.setState({lastRecording: result})
+          // Alert.alert("Video recorded!", JSON.stringify(result));
+          this.setState({lastRecording: result})
+    
+          if(this.state.saveToCameraRoll) {
+            CameraRoll.saveToCameraRoll(result.uri, "video").then(() => {
+              console.log("Video saved to camera roll successfuly");
+            }).catch((err) => {
+              console.warn("Failed to store recorded video: " + err.message);
+            });
           }
-  
-          // give time for the camera to recover
-          setTimeout(()=>{
-            this.setState({recording: false});
-          }, 500);
-  
-          // might be cleared on recording stop or
-          // here if we had errors
-          if(this._recordingTimer){
-            clearInterval(this._recordingTimer);
-            this._recordingTimer = null;
-          }
-  
-          CameraRoll.saveToCameraRoll(result.uri, "video");
-        }, () => {
-          console.log("VIDEO RECORD FAIL", err.message, err);
-          Alert.alert("Error", "Failed to store recorded video: " + err.message);
+
+        }).catch((err) => {
+          console.warn("Video recording error. err=" + err);
         })
 
-        console.log(new Date().getTime() + ' launch video playback')
+        console.log("Playback delay: " + startVideoDelay + "ms")
 
         setTimeout(()=>{
           this.startVideo()
@@ -267,40 +258,33 @@ class App extends Component {
   }
 
   stopRecording = () => {
-    console.log("STOP RECORDING!")
-    if(this.camera && this.state.recording){
+    console.log("STOP RECORDING")
+    if(this.camera && this.state.state=='recording'){
       this.camera.stopRecording();
     }
   }
 
   startVideo = (e) => {
-    var start = new Date().getTime()
     this.setState({paused: false}, async () => {
       console.log(new Date().getTime() + " startVideo")
-      console.log("setState took " + (new Date().getTime() - start) + "ms")
     })
-    // console.log(this.player1)
-    // this.player1.resume()
   }
 
   stopVideo = (e) => {
-    this.setState({paused: true})   
-    this.player1.seek(0)
-  }
-
-  playRecorded = (e) => {
-    this.player1.seek(0)
-    this.player3.seek(0)
-    setTimeout(()=>{
-      this.startVideo()
-    }, 500);
+    this.setState({paused: true}, async () => {
+      console.log(new Date().getTime() + " stopVideo")
+      this.player1.seek(0)
+      if(this.player2) {
+        this.player2.seek(0)
+      }
+    })
   }
 
   togglePlayRecorded = async () => {
     if (!this.state.paused) {
-      this.setState({paused: true})
+      this.stopVideo()
     } else {
-      this.playRecorded()
+      this.startVideo()
     }
   }
 
@@ -316,8 +300,8 @@ class App extends Component {
   }
 
   onRecordingStart = (e) => {
-    this.state.isRecording = true
-    this.state.lastRecordingActualTime = new Date().getTime(),
+    this.state.state = 'recording'
+    this.state.lastRecordingActualStartTime = new Date().getTime(),
     // this.state.recordingTriggerTime = (this.state.lastRecordingActualTime - this.state.lastRecordingTime)
     console.log(new Date().getTime() + ' onRecordingStart')
     // console.log('onRecordingStart: trigger time: ' + this.state.recordingTriggerTime + 'ms')
@@ -326,7 +310,7 @@ class App extends Component {
 
   onRecordingEnd = (e) => {
     console.log(new Date().getTime() + ' onRecordingEnd')
-    this.state.isRecording = false
+    this.state.state = 'idle'
   }
 
   onCameraReady = (e) => {
@@ -346,19 +330,15 @@ class App extends Component {
     console.log(new Date().getTime() + ' onLoadStart')
   }
   onProgress = (e) => {
-    console.log(new Date().getTime() + ' onProgress')
     var playerElapsed = e.currentTime * 1000
 
-    if(this.state.isRecording) {
-      var recorderElapsed = (new Date().getTime() - this.state.lastRecordingActualTime)
-      if(this.state.lagCounter<5) {
+    if(this.state.state=='recording') {
+      var recorderElapsed = (new Date().getTime() - this.state.lastRecordingActualStartTime)
+      if(this.state.lagCounter<10) {
+        console.log(new Date().getTime() + ' onProgress measure lag - ' + (playerElapsed - recorderElapsed) + "ms")
         this.state.lagCounter++
         this.state.lagTotal += (playerElapsed - recorderElapsed)
       }
-      // console.log("PLAYER   ELAPSED " + playerElapsed + "ms")
-      // console.log("RECORDER ELAPSED " + recorderElapsed + "ms")
-      // console.log("LAG ADJUST=" + this.state.lastLag)
-      console.log("CURRENT LAG=" + playerElapsed - recorderElapsed)
     }
   }
   onReadyForDisplay = (e) => {
@@ -407,13 +387,17 @@ const styles = StyleSheet.create({
     paddingRight: 12,
     textAlign: 'right',
   },
+  camera: {
+    paddingTop: 10,
+    height: 250,
+  },
   video1: {
     paddingTop: 10,
-    height: 150,
+    height: 250,
   },
-  video3: {
+  video2: {
     paddingTop: 10,
-    height: 300,
+    height: 250,
   },
 });
 
