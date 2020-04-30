d91966090eba638b463e4861f847982beb95647c
flaviostutz <flaviostutz@gmail.com>
2020-04-30 11:40:30 -0300

    Just after applying video trimmer manual install (this diff may be used for npm install scripting for them later)

---
+++ Recco/ios/Recco-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Recco-Bridging-Header.h
+//  Recco
+//
+//  Created by Flavio de Oliveira Stutz on 4/30/20.
+//
+
+#ifndef Recco_Bridging_Header_h
+#define Recco_Bridging_Header_h
+
+
+#endif /* Recco_Bridging_Header_h */
